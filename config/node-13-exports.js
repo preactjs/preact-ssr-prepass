b85e37eb51f993f6257768f49148e0816e68e9ab
Den Ilin <followdarko@gmail.com>
2021-07-09 19:58:37 +0300

    Add mjs support (#30)
    * Add mjs support
    
    * FIx typos

---
+++ config/node-13-exports.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const snakeCaseToCamelCase = str =>
+	str.replace(/([-_][a-z])/g, group => group.toUpperCase().replace('-', ''));
+
+const copy = name => {
+	// Copy .module.js --> .mjs for Node 13 compat.
+	const filename = name.includes('-') ? snakeCaseToCamelCase(name) : name;
+	fs.writeFileSync(
+		`${process.cwd()}/dist/${filename}.mjs`,
+		fs.readFileSync(`${process.cwd()}/dist/${filename}.module.js`)
+	);
+};
+
+copy('index');
